96b5707e0f4d296a6c637a484bb3aead2781fceb
Shala <s00043039@acad.tri-c.edu>
2020-11-30 01:58:27 -0500

    Add project files.

---
+++ LAB 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LAB_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//A one-dimensional array p contains four elements.  The array access expressions to access each of the elements in p are _0______, 1_______,2_______ and 3______.
+int[] Months = new int[12];
+for ( int counter= Months; counter <January-December; ++counter)
+           12
+        Console.WriteLine($" {conter,12} {Array[counter],12}");
+//four element array to store the seasons
+int[] Seasons = new int[4]
+    for (int counter= Seasons; counter <Winter, Spring, Summer, Fall; ++counter)
+          4
+    Console.WriteLine($"{counter,4} {Array[counter],4}");
+string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+int i = 0;
+while (i < names.Length)
+{
+    Console.WriteLine(names[0]);
+}
+Console.WriteLine("{0,2}. {1}", i, names[i]);
